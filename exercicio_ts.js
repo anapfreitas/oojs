e3293f07d9495619092c64965c6734ac9f7272e2
Ana Paula Freitas <103225141+anapfreitas@users.noreply.github.com>
2025-08-13 10:55:38 -0300

    Add files via upload

---
+++ exercicio_ts.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicacaoDoisNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacaoDoisNumeros(10, 20));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('ana'));
+//# sourceMappingURL=exercicio_ts.js.map
